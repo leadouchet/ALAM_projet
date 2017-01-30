506c3abe5ad500b7c565262f34219b3d425b247a
ldouchet <ldouchet@hotmail.fr>
2017-01-30 19:25:23 +0100

    epsilon et t

--- diffusion_map.py
+++ diffusion_map.py
@@ -15,19 +15,20 @@ def propre(x, eps): #x est une matrice type numpy
 	L=np.zeros((nb_ind,nb_ind))
 	for i in range(nb_ind) :
 		for j in range(i,nb_ind) :
-			L[i][j]=math.exp(-np.linalg.norm(x[i]-x[j])**2/eps)
-			L[j][i]=L[i][j]
-
-
+			L[i,j]=math.exp(-np.linalg.norm(x[i]-x[j])**2/eps)
+			L[j,i]=L[i,j]
 	
 	#normalisation de la matrice L
 
 	# On commence par creer la matrice D, diagonale et positive
 	D=np.zeros(np.shape(L))
 	Dp=np.zeros(np.shape(L))
+	Dg=np.zeros(np.shape(L))
 	for i in range(nb_ind) :
-		D[i,i]=sum(L[i]) 
-		Dp[i][i]=D[i,i]**(-0.5)
+		D[i,i]=sum(L[i,:]) 
+		Dp[i,i]=D[i,i]**(-0.5)
+		Dg[i,i]=D[i,i]**(0.5)
+
 
 
 	#creation de la matrice M normalisee
@@ -41,6 +42,7 @@ def propre(x, eps): #x est une matrice type numpy
 	#calcule des valeurs propres
 	valpropre,vecpropre=np.linalg.eig(Ms)
 
+
 	#trie les vecteurs propres dans l ordre decroissants de la valeur absolue de leur valeur propres
 	tri=np.concatenate(([valpropre],vecpropre),axis=0)
 	valpropre_ordre=sorted(valpropre,reverse=True)
@@ -53,20 +55,17 @@ def propre(x, eps): #x est une matrice type numpy
 
 
 	#decomposition de la matrice M=phi*V*rho
-	psi=np.dot(np.linalg.inv(np.sqrt(D)),vecpropre)
-	phi=np.dot(np.sqrt(D),np.transpose(vecpropre)) #np.linalg.inv(vecpropre) ne fonctionne pas det=0
+	psi=np.dot(Dp,vecpropre)
+	phi=np.dot(Dg,np.transpose(vecpropre)) #np.linalg.inv(vecpropre) ne fonctionne pas det=0
 	return (valpropre,psi,phi)
-
 
 
 def diffusion_map(vecproprepsi,valpropre,k,t):
 	psit=np.zeros((len(vecproprepsi),k))
 	for i in range(k) :
-		psit[:,i]=(valpropre[i+1]**t)*vecproprepsi[:,i+1]
+		psit[:,i]=(valpropre[i+1]**t)*np.array(vecproprepsi[:,i+1])
 	return psit
 
-
-#df = pd.read_csv("dCt_values.tab",sep='\t',decimal=",",header=0,index_col=0 )
 
 #normaliser les donnees
 
@@ -92,8 +91,8 @@ with open(fileName, 'rb') as f:
 
 x=np.array(data)
 
-res=propre(x,0.1)
-diff=diffusion_map(res[1],res[0],4, 10)
+res=propre(x,150)
+diff=diffusion_map(res[1],res[0],4, 2)
 
 
 
@@ -101,6 +100,11 @@ diff=diffusion_map(res[1],res[0],4, 10)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(diff[:,0], diff[:,1],diff[:,3])
+ax.scatter(diff[:,3],diff[:,1],diff[:,0])
+plt.ylabel('dimension 2')
+plt.xlabel("dimension 4")
 plt.show()
 
+#plt.plot(diff[:,0],diff[:,1])
+#plt.ylabel('dimension 1')
+#plt.xlabel("dimension 2")
